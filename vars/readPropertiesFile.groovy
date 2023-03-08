1e995984d0fbeda9bc16e8ff50d448120270ad9a
Ivan Klen <ivan.klen5@gmail.com>
2023-03-08 23:23:52 +0100

    repl

--- vars/readPropertiesFile.groovy
+++ vars/readPropertiesFile.groovy
@@ -1,5 +1,23 @@
+// vars/readPropertiesFile.groovy
+
 def call(String filePath) {
   def props = new Properties()
+  fileExistsOrDie(filePath)
   props.load(new FileInputStream(filePath))
-  return props
+
+  // Set each property as an environment variable
+  props.each { key, value ->
+    env."${key}" = "${value}"
+  }
+}
+
+def fileExistsOrDie(String filePath) {
+  if (!fileExists(filePath)) {
+    throw new FileNotFoundException("File not found: ${filePath}")
+  }
+}
+
+def fileExists(String filePath) {
+  def file = new File(filePath)
+  return file.exists()
 }
